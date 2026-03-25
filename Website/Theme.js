0a6c35b24989411fac556fa083300b4cd03f0473
Lunam2027 <167571311+Lunam2027@users.noreply.github.com>
2026-03-25 00:18:09 -0600

    add more themes and pictures

--- Website/Theme.js
+++ Website/Theme.js
@@ -1,10 +1,9 @@
 var root = document.querySelector(":root");
 
-
 var mint = document.getElementById("mint");
 function setMintTheme() {
     var mintTitle = "#05385b";
-    var mintGrid = "#379683";
+    var mintGrid = "#20688a";
     var mintBackground = "#5cdb95";
     var mintText = "#edf5e1";
     root.style.setProperty("--titleColor", mintTitle);
@@ -25,7 +24,110 @@ function setWineTheme() {
     root.style.setProperty("--textColor", wineText);
 }
 
+var ezMode = document.getElementById("ez-mode");
+function setEzTheme() {
+    var ezTitle = "#111111";
+    var ezGrid = "#888888";
+    var ezBackground = "#b9b9b9";
+    var ezText = "#eeeeee";
+    root.style.setProperty("--titleColor", ezTitle);
+    root.style.setProperty("--gridColor", ezGrid);
+    root.style.setProperty("--backgroundColor", ezBackground);
+    root.style.setProperty("--textColor", ezText);
+}
 
+var miami = document.getElementById("miami");
+function setMiamiTheme() {
+    var miamiTitle = "#1a1a2e";
+    var miamiGrid = "#08d9d6";
+    var miamiBackground = "#ff2e63";
+    var miamiText = "#eaeaea";
+    root.style.setProperty("--titleColor", miamiTitle);
+    root.style.setProperty("--gridColor", miamiGrid);
+    root.style.setProperty("--backgroundColor", miamiBackground);
+    root.style.setProperty("--textColor", miamiText);
+}
 
+var honey = document.getElementById("honey");
+function setHoneyTheme() {
+    var honeyTitle = "#2b1d0e";
+    var honeyGrid = "#fb8500";
+    var honeyBackground = "#ffb703";
+    var honeyText = "#fff3b0";
+    root.style.setProperty("--titleColor", honeyTitle);
+    root.style.setProperty("--gridColor", honeyGrid);
+    root.style.setProperty("--backgroundColor", honeyBackground);
+    root.style.setProperty("--textColor", honeyText);
+}
 
+var sweden = document.getElementById("sweden");
+function setSwedenTheme() {
+    var swedenTitle = "#002f6c";
+    var swedenGrid = "#0057b7";
+    var swedenBackground = "#ffcc00";
+    var swedenText = "#ffffff";
+    root.style.setProperty("--titleColor", swedenTitle);
+    root.style.setProperty("--gridColor", swedenGrid);
+    root.style.setProperty("--backgroundColor", swedenBackground);
+    root.style.setProperty("--textColor", swedenText);
+}
 
+var sewingTin = document.getElementById("sewing-tin");
+function setSewingTheme() {
+    var sewingTitle = "#2e2e2e";
+    var sewingGrid = "#f46036";
+    var sewingBackground = "#d7263d";
+    var sewingText = "#f7f7ff";
+    root.style.setProperty("--titleColor", sewingTitle);
+    root.style.setProperty("--gridColor", sewingGrid);
+    root.style.setProperty("--backgroundColor", sewingBackground);
+    root.style.setProperty("--textColor", sewingText);
+}
+
+var antihero = document.getElementById("antihero");
+function setAntiheroTheme() {
+    var antiheroTitle = "#0f0f0f";
+    var antiheroGrid = "#457b9d";
+    var antiheroBackground = "#e63946";
+    var antiheroText = "#f1faee";
+    root.style.setProperty("--titleColor", antiheroTitle);
+    root.style.setProperty("--gridColor", antiheroGrid);
+    root.style.setProperty("--backgroundColor", antiheroBackground);
+    root.style.setProperty("--textColor", antiheroText);
+}
+
+var taro = document.getElementById("taro");
+function setTaroTheme() {
+    var taroTitle = "#2b213a";
+    var taroGrid = "#b5838d";
+    var taroBackground = "#cdb4db";
+    var taroText = "#f5e6ff";
+    root.style.setProperty("--titleColor", taroTitle);
+    root.style.setProperty("--gridColor", taroGrid);
+    root.style.setProperty("--backgroundColor", taroBackground);
+    root.style.setProperty("--textColor", taroText);
+}
+
+var creamsicle = document.getElementById("creamsicle");
+function setCreamsicleTheme() {
+    var creamsicleTitle = "#fff1e6";
+    var creamsicleGrid = "#ffb703";
+    var creamsicleBackground = "#ff7f11";
+    var creamsicleText = "#3a0ca3";
+    root.style.setProperty("--titleColor", creamsicleTitle);
+    root.style.setProperty("--gridColor", creamsicleGrid);
+    root.style.setProperty("--backgroundColor", creamsicleBackground);
+    root.style.setProperty("--textColor", creamsicleText);
+}
+
+var matchaMocha = document.getElementById("matcha-mocha");
+function setMatchaTheme() {
+    var matchaTitle = "#2d1e1e";
+    var matchaGrid = "#6f4e37";
+    var matchaBackground = "#a3b18a";
+    var matchaText = "#fefae0";
+    root.style.setProperty("--titleColor", matchaTitle);
+    root.style.setProperty("--gridColor", matchaGrid);
+    root.style.setProperty("--backgroundColor", matchaBackground);
+    root.style.setProperty("--textColor", matchaText);
+}
