ef3b21fd4e5ed843378183e591e9a9daca89ffa4
Lyrae.zip <milesluna744@gmail.com>
2026-04-09 20:14:51 -0600

    add gradient

--- Website/Theme.js
+++ Website/Theme.js
@@ -40,6 +40,54 @@ function removeSelected() {
     });
 }
 
+function rgbToHex(rgb) {
+    const parts = rgb.match(/\d+/g);
+    return "#" + parts.map(x => 
+        parseInt(x).toString(16).padStart(2, "0")
+    ).join("");
+}
+
+// https://gist.github.com/xenozauros/f6e185c8de2a04cdfecf?permalink_comment_id=4193442#gistcomment-4193442
+function hexToCssHslAnalagous(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  var r = parseInt(result[1], 16);
+  var g = parseInt(result[2], 16);
+  var b = parseInt(result[3], 16);
+
+  r /= 255; g /= 255; b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0;
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch(max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  h = Math.round(h * 360);
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  let h1 = (h - 15 + 360) % 360;
+  let h2 = (h + 15) % 360;
+
+  return [
+    `hsl(${h1}, ${s}%, ${l}%)`,
+    `hsl(${h2}, ${s}%, ${l}%)`
+  ];
+}
+
+
 function setTheme(event){
     removeSelected();
 
@@ -54,6 +102,23 @@ function setTheme(event){
     }
     
     for (let i = 0; i < colours.length; i++) {
+        if (i === 0){
+            let baseColor = event.currentTarget.style.backgroundColor;
+
+            let string = "linear-gradient(337deg, ";
+            string += baseColor + " 0%, ";
+
+            let hex = rgbToHex(baseColor);
+
+            let [hsl1, hsl2] = hexToCssHslAnalagous(hex);
+
+            string = "linear-gradient(337deg, ";
+            string += hsl1 + " 0%, ";
+            string += baseColor + " 50%, ";
+            string += hsl2 + " 100%)";
+
+            colourarray[0] = string;
+        }
         colourarray.push(colours[i].style.backgroundColor);
     }
     event.currentTarget.classList.add("selectedTheme");
